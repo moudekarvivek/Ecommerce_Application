c26762df2c926f54bd802fb6c984d48ddeeb3d4c
moudekarvivek <moudekarvivek@gmail.com>
2024-11-29 12:25:52 +0530

    Logic for Deleting the Category object from the list of Categories

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,10 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +12,7 @@ public class CategoryController {
 
     private CategoryService categoryService;
 
+    // Constructor injection
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
@@ -27,5 +25,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return "Category Added Successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}") // Because Id is coming here through pathvariable
+    public String deleteCategory(@PathVariable Long categoryId){
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
-    private  Long nextId = 1L;
+    private  Long nextId = 1L; // Used to get the ID automatically
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -18,5 +18,16 @@ public class CategoryServiceImpl implements CategoryService{
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()  // Making Categories list into stream
+                .filter(c -> c.getCategoryId().equals(categoryId)) //we are doing Functional Programming here, and checking the categoryid matches the category we have
+                .findFirst().orElse(null);
+        if(category == null)
+            return "Category Not Found";
+        categories.remove(category);
+        return "Category With categoryId: " + categoryId + " deleted Successfully!!";
     }
 }
