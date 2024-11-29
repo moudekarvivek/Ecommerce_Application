22b4f04a3be23e4d59fdd550ed1eec4b47bf7cc8
moudekarvivek <moudekarvivek@gmail.com>
2024-11-29 11:05:49 +0530

    Managing ID's Automatic

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
-
+    private  Long nextId = 1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -16,6 +16,7 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 }
