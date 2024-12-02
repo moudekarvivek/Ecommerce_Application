e9aef90a4c6a741759697e802cb9507c90d1faad
moudekarvivek <moudekarvivek@gmail.com>
2024-12-02 16:38:47 +0530

    Used GlobalExceptionHandler to handle the custom Exceptions

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    
+    @NotBlank
+    @Size(min = 5, message = "Category Name must contain atleast 5 characters")
     private String categoryName;
 
 }
