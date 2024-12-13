e5714572b94c3e82c7d10bacba17858bc7fad1ae
moudekarvivek <moudekarvivek@gmail.com>
2024-12-13 17:30:11 +0530

    Creating AuthController and adding Signin and SignUp Endpoint

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,17 +1,32 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security.response;
 
 import java.util.List;
 
-public class LoginResponse {
+public class UserInfoResponse {
+    private Long id;
     private String jwtToken;
-
     private String username;
     private List<String> roles;
 
-    public LoginResponse(String username, List<String> roles, String jwtToken) {
+    public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
+        this.id = id;
         this.username = username;
         this.roles = roles;
         this.jwtToken = jwtToken;
+    }
+
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getJwtToken() {
@@ -38,5 +53,4 @@ public class LoginResponse {
         this.roles = roles;
     }
 }
-
 
